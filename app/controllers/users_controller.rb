f34549c76f14e279a4c2ea01a1a3991cbb3fd57f
Sho Emoto <dd2d00seg1@gmail.com>
2022-02-13 19:42:29 +0900

    controllerを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: { data: users }
+    end
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            render json: user, status: :created
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:name)
+    end
 end
